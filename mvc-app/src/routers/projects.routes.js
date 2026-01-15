686ba57cdabd00b5ea18d69060b4ab045c480512
Josh Archer <jarcher@greenriver.edu>
2026-01-15 10:34:53 -0800

    Added another controller method.

--- mvc-app/src/routers/projects.routes.js
+++ mvc-app/src/routers/projects.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllProjects, getSingleProject } from './../controllers/projects.controller.js'
+import { getAllProjects, getSingleProject, getTechRelated } from './../controllers/projects.controller.js'
 
 const router = Router();
 
 router.get("/all", getAllProjects);
 router.get("/single", getSingleProject);
+router.get("/single/tech", getTechRelated);
 
 export default router;
